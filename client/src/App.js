6ae64c68983b766c8551b1dce2b14d7335faf32f
tchoutdara <monkeymilitia444@gmail.com>
2018-11-09 15:41:48 -0700

    setting theme

--- client/src/App.js
+++ client/src/App.js
@@ -3,27 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 import {
   Header,
-  Container
+  Container, 
+  Divider
 } from 'semantic-ui-react';
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-const BlackBody = styled.div`
-  font-family: 'K2D', sans-serif;
-  background-color: black;
-  color: white;
+const Theme = styled.div`
+  background-color: white;
+  height: 100%;
   background-size: cover;
-  height: 1000px;
 `
 
 class App extends Component {
   render() {
     return (
-      <BlackBody>
-        <Container>
-        <Header as='h1' color='teal'>Tony Choutdara</Header>
-        <Header as='h3' color='teal'>Web Developer</Header>
-        </Container>
-      </BlackBody>
+      <Container>
+      <Theme>
+            <Header as='h1' color='teal' style={{textAlign:'center', marginTop:'35px'}}>Hello my name is Tony Choutdara and I am a Web Developer</Header>
+            <Header as='h3' color='teal' style={{textAlign:'center'}}>Ruby  |  Javascript  |  PostgreSQL</Header>
+      </Theme>
+      </Container>
     );
   }
 }
